a9ddc54de1c5d10c061713fcdf13f32b93bea470
Cao Khắc Bảo <87173948+devbaoo@users.noreply.github.com>
2026-02-11 14:55:05 +0700

    fix: update size of slide (#50)

--- src/models/audioRecord.js
+++ src/models/audioRecord.js
@@ -1,30 +1,48 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class AudioRecord extends Model {
-        static associate(models) {
-            AudioRecord.belongsTo(models.Presentation, { foreignKey: 'presentationId', as: 'presentation' });
-            AudioRecord.hasOne(models.Transcript, { foreignKey: 'audioId', as: 'transcript' });
-        }
+  class AudioRecord extends Model {
+    static associate(models) {
+      AudioRecord.belongsTo(models.Presentation, {
+        foreignKey: "presentationId",
+        as: "presentation",
+      });
+      AudioRecord.hasOne(models.Transcript, {
+        foreignKey: "audioId",
+        as: "transcript",
+      });
     }
+  }
 
-    AudioRecord.init(
-        {
-            audioId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-            presentationId: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+  AudioRecord.init(
+    {
+      audioId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      presentationId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
 
-            filePath: { type: DataTypes.TEXT, allowNull: false },
-            fileName: { type: DataTypes.TEXT },
-            fileFormat: { type: DataTypes.STRING(20) },
-            fileSizeBytes: { type: DataTypes.BIGINT },
-            durationSeconds: { type: DataTypes.INTEGER },
-            sampleRate: { type: DataTypes.INTEGER },
-            recordingMethod: { type: DataTypes.STRING(50) },
-            uploadedAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-        },
-        { sequelize, modelName: 'AudioRecord', tableName: 'AudioRecords' }
-    );
+      filePath: { type: DataTypes.TEXT, allowNull: false },
+      fileName: { type: DataTypes.TEXT },
+      fileFormat: { type: DataTypes.STRING(20) },
+      fileSizeBytes: { type: DataTypes.BIGINT },
+      durationSeconds: { type: DataTypes.INTEGER },
+      sampleRate: { type: DataTypes.INTEGER },
+      recordingMethod: { type: DataTypes.STRING(50) },
+      uploadedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    { sequelize, modelName: "AudioRecord", tableName: "AudioRecords" }
+  );
 
-    return AudioRecord;
+  return AudioRecord;
 };
